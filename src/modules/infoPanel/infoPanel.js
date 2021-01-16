87d66de7082f30e9ca0c1c343b1f8cc255d9f9f6
Or Schneider <contact@SchneiderOr.com>
2021-01-16 18:22:03 +0200

    fixes

--- src/modules/infoPanel/infoPanel.js
+++ src/modules/infoPanel/infoPanel.js
@@ -39,7 +39,7 @@ const Wrapper = styled.div`
   position: relative;
   display: flex;
   width: 100%;
-  height: 200px;
+  min-height: 200px;
   margin-top: 30px;
   align-items: center;
   white-space: pre;
